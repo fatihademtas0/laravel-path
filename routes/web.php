81471785a751a927eb692f9f5fd192d527ef6d67
fatihademtas0 <fatihademtas@outlook.com>
2024-11-21 03:24:53 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home' , [
+        "greeting" => "Hello",
+        "name" => "Fatih"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
