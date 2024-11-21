01cbcbe13de5280c815b81e41c44176cfef0018d
fatihademtas0 <fatihademtas@outlook.com>
2024-11-22 02:35:09 +0300

    migration

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -7,7 +7,7 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
-   
+
 <div class="min-h-full">
   <nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -20,7 +20,7 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-              <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+              <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
               <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
             </div>
           </div>
@@ -71,7 +71,7 @@
       <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
         <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-        <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+        <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jobs</a>
         <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
       </div>
       <div class="border-t border-gray-700 pb-3 pt-4">
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
-    return view('home' , [
-        "greeting" => "Hello",
-        "name" => "Fatih"
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs' , [
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
