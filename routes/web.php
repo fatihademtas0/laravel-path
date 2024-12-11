e298ab928f3740f1164ef473694623da31c1c95e
fatihademtas0 <fatihademtas@outlook.com>
2024-12-12 00:50:10 +0300

    auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/' , 'home');
@@ -28,5 +29,11 @@ Route::controller(JobController::class) ->group(function () {
 
 //auth
 Route::get('/register', [RegisteredUserController::class , 'create']);
+Route::post('/register', [RegisteredUserController::class , 'store']);
+
+Route::get('/login' , [SessionController::class , 'create']);
+Route::post('/login' , [SessionController::class , 'store']);
+
+
 
 
