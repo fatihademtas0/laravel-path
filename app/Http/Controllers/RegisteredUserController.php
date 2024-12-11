d5cb96ee29d05be2ab028eb8d570ca7e4604a8b5
fatihademtas0 <fatihademtas@outlook.com>
2024-12-12 02:01:02 +0300

    auth

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +16,17 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        $attributes = request()->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', Password::min(6), 'confirmed'],
+            ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,15 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
+
+    protected $guarded = [];
+
+    /*protected $fillable = [
+        'first_name',
+        'last_name',
         'email',
         'password',
-    ];
+    ];*/
 
     /**
      * The attributes that should be hidden for serialization.
